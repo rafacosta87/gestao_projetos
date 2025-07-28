14ed28a70a4e688c7c5368bd41f8c73e69d1a1bf
rafael <silva.rafa@hotmail.com>
2025-07-28 20:09:41 -0300

    Algumas melhorias

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 //ver a questão do teste no terminal, pedir os comandos, e se ele trabalha com mais de um termina, que no caso é o  código a frente. /Exemplo de comando CURL: curl -X PUT "http://localhost:3000/tarefa/atualizar" -H "Content-Type: application/jso?id=-1" -d '{"descricao": "Declaracoes", "id_colaborador": 3}'
 //falar para o professor mostrar aquelas operações que ele fez no terminal , referente null e undefined. Pedir para Jadson explicar melhor aquele teste que ele fez , para ver se estava imprimindo o hello word, entre as linhas do codigo
 //a pergunta 3 no final do código  ver com Jadson como da para resolver aquela questão só usando o mysql 
+//Nestes endpoint que recebem 2 ids , supondo id_colaborador , id_projeto e id_equipe, se não podemos fazer um zod que receba primeiro_id e segundo_id. Assim podemos aplicar ele em varios endpoints.
+//depois separar projeto por pastas
 
 const { z } = require('zod')
 const express = require('express');
@@ -13,6 +15,18 @@ app.use(express.json());
 
 const departamentos = [
     "TI", "Financeiro", "RH", "Comercial"
+]
+
+const opcoesStatus = [
+    "pendente", "andamento", "concluido"
+]
+
+const opcoesPrioridades = [
+    "baixa", "media", "alta"
+]
+
+const opcoesCargo = [
+    "Backend", "Frontend", "Marketing", "Contador", "Financeiro", "Vendedor"
 ]
 
 const BodyCadastroColaboradorSchema = z.object({
@@ -453,6 +467,8 @@ app.delete('/equipe/deletar', async (req, res) => {
     }
 })
 
+
+
 const BodyCadastroTarefaSchema = z.object({
     id_projeto: z.number({
         invalid_type_error: "Id precisa ser de tipo numérico",
@@ -470,19 +486,9 @@ const BodyCadastroTarefaSchema = z.object({
     })
         .min(1, { message: "Não pode ter menos de 1 caractere" })
         .max(20, { message: "Não pode ter mais de 20 caracteres" }),
-    status: z.string({
-        invalid_type_error: "Status precisa ser do tipo string",
-        required_error: "Campo obrigatório"
-    })
-        .min(1, { message: "Não pode ter menos de 1 caractere" })
-        .max(20, { message: "Não pode ter mais de 20 caracteres" })
+    status: z.enum(opcoesStatus, { message: "Status inválido" })
         .optional(),
-    prioridade: z.string({
-        invalid_type_error: "Prioridade precisa ser do tipo string",
-        required_error: "Campo obrigatório"
-    })
-        .min(1, { message: "Não pode ter menos de 1 caractere" })
-        .max(20, { message: "Não pode ter mais de 20 caracteres" })
+    prioridade: z.enum(opcoesPrioridades, { message: "Prioridade inválida" })
         .optional(),
     data_termino: z.string({ message: "Campo obrigatório" })
         .regex(/^\d{4}-\d{2}-\d{2}$/, { message: "Data inválida" }),
@@ -501,7 +507,7 @@ app.post('/tarefa/cadastrar', async (req, res) => {
         if (verificacaoBody.success == false) return res.status(422).json({ error: verificacaoBody.error.format() });
         const { id_projeto, id_colaborador, titulo, status, prioridade, data_termino, descricao } = req.body;
         const [row] = await db.query(
-            "select Alocacoes.id_equipe from Membros  inner join Equipes on Equipes.id = Membros.id_equipe inner join Alocacoes on Equipes.id = Alocacoes.id_equipe where id_projeto= ? and id_colaborador= ?;"
+            "select Alocacoes.id_equipe from Membros inner join Alocacoes on Membros.id_equipe = Alocacoes.id_equipe where id_projeto= ? and id_colaborador= ?"
             , [id_projeto, id_colaborador]
         );
         if (row.length < 1) {
@@ -590,19 +596,9 @@ const BodyAtualizarTarefaSchema = z.object({
         .min(1, { message: "Não pode ter menos de 1 caractere" })
         .max(20, { message: "Não pode ter mais de 20 caracteres" })
         .optional(),
-    status: z.string({
-        invalid_type_error: "Status precisa ser do tipo string",
-        required_error: "Campo obrigatório"
-    })
-        .min(1, { message: "Não pode ter menos de 1 caractere" })
-        .max(20, { message: "Não pode ter mais de 20 caracteres" })
+    status: z.enum(opcoesStatus, { message: "Status inválido" })
         .optional(),
-    prioridade: z.string({
-        invalid_type_error: "Prioridade precisa ser do tipo string",
-        required_error: "Campo obrigatório"
-    })
-        .min(1, { message: "Não pode ter menos de 1 caractere" })
-        .max(20, { message: "Não pode ter mais de 20 caracteres" })
+    prioridade: z.enum(opcoesPrioridades, { message: "Prioridade inválida" })
         .optional(),
     data_termino: z.string({ message: "Campo obrigatório" })
         .regex(/^\d{4}-\d{2}-\d{2}$/, { message: "Data inválida" })
@@ -683,12 +679,7 @@ const BodyCadastroMembroSchema = z.object({
         required_error: "Campo obrigatório"
     })
         .min(1, { message: "Id tem que ter pelo menos 1 digito" }),
-    cargo: z.string({
-        invalid_type_error: "Cargo precisa ser do tipo string",
-        required_error: "Campo obrigatório"
-    })
-        .min(1, { message: "Não pode ter menos de 1 caractere" })
-        .max(20, { message: "Não pode ter mais de 20 caracteres" }),
+    cargo: z.enum(opcoesCargo, { message: "Cargo inválido" }),
 })
 
 app.post('/colaborador/equipe/cadastrar', async (req, res) => {
@@ -806,22 +797,7 @@ const QueryMembroIdSchema = z.object({
 })
 
 const BodyAtualizarMembroSchema = z.object({
-    id_colaborador: z.number({
-        invalid_type_error: "Id precisa ser de tipo numérico",
-        required_error: "Campo obrigatório"
-    })
-        .min(1, { message: "Id tem que ter pelo menos 1 digito" })
-        .optional(),
-    id_equipe: z.number({
-        invalid_type_error: "Id precisa ser de tipo numérico",
-        required_error: "Campo obrigatório"
-    })
-        .min(1, { message: "Id tem que ter pelo menos 1 digito" })
-        .optional(),
-    cargo: z.string({
-        invalid_type_error: "Cargo precisa ser do tipo string"
-    })
-        .max(20, { message: "Não pode ter mais de 20 caracteres" })
+    cargo: z.enum(opcoesCargo, { message: "Cargo inválido" })
         .optional()
 })
 
@@ -880,12 +856,12 @@ const BodyCadastroAlocacaoSchema = z.object({
         invalid_type_error: "Id precisa ser de tipo numérico",
         required_error: "Campo obrigatório"
     })
-        .min(1, { message: "Id tem que ter pelo menos 1 digito" }),
+        .min(1, { message: "Id tem que ser maior que 0" }),
     id_projeto: z.number({
         invalid_type_error: "Id precisa ser de tipo numérico",
         required_error: "Campo obrigatório"
     })
-        .min(1, { message: "Id tem que ter pelo menos 1 digito" }),
+        .min(1, { message: "Id tem que ser maior que 0" }),
 })
 
 app.post('/equipe/projeto/cadastrar', async (req, res) => {
@@ -1083,6 +1059,24 @@ app.get('/carga/trabalho/colaborador', async (req, res) => {
     }
 });
 
+app.get('/projeto/consultar/cargos', async (req, res) => {
+    try {
+        const verificacaoQuery = QueryConsultaIdSchema.safeParse(req.query)
+        if (verificacaoQuery.success == false) return res.status(422).json({ error: verificacaoQuery.error.format() });
+        const { id } = req.query;
+        const [row] = await db.query('select Membros.cargo from Alocacoes inner join Membros on Membros.id_equipe = Alocacoes.id_equipe where Alocacoes.id_projeto= ? ', [id])
+        if (row.length < 1) {
+            // Status: Not Found
+            return res.status(404).json(`Não tem nenhum cargo neste projeto.`);
+        }
+        return res.status(200).json(row);
+    }
+    catch (e) {
+        return res.status(500).json(e);
+    }
+});
+
+
 
 app.listen(3000, () => {
     console.log('Servidor rodando em http://localhost:3000');
@@ -1091,20 +1085,24 @@ app.listen(3000, () => {
 
 
 //1-Listar tarefas por projeto , colaborador e status.
-//select Projetos.nome as "Projetos", Tarefas.titulo as "Tarefas", Colaboradores.nome as "Colaboradores", Tarefas.status from Projetos
-//inner join Tarefas on Projetos.id = Tarefas.id_projeto 
-//inner join Colaboradores on Colaboradores.id = Tarefas.id_colaborador where Projetos.id = ?;
+//select Tarefas.titulo as "Tarefas", Colaboradores.nome as "Colaboradores", Tarefas.status from Tarefas
+//inner join Colaboradores on Colaboradores.id = Tarefas.id_colaborador 
+//where id_projeto = ?;
 
 //2-Atualizar status da tarefa (pedente, andamento, concluida)
 //update Tarefas set status = "concluido" where id= {id_projeto};
 
 //3-Não perimitir que uma tarefa seja criada para um colaborador que não faz parte do projeto.
 //select Alocacoes.id_equipe from Membros 
-//inner join Equipes on Equipes.id = Membros.id_equipe
-//inner join Alocacoes on Equipes.id = Alocacoes.id_equipe
+//inner join Alocacoes on Membros.id_equipe = Alocacoes.id_equipe 
 //where id_projeto= ? and id_colaborador= ?
 
 //4-Visualizar carga de trabalho de um colaborador(tarefas atribuidas).
 //select (24 * (datediff(curdate(), data_inicio ))) as 'Carga de Trabalho'  from Tarefas  
 //inner join Projetos on Tarefas.id_projeto = Projetos.id  
 //where id_colaborador = ? and id_projeto= ? ;
+
+//5-Consultar cargos por projeto
+//select Membros.cargo from Alocacoes 
+//inner join Membros on Membros.id_equipe = Alocacoes.id_equipe 
+//where Alocacoes.id_projeto= ? 
